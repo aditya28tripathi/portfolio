77541ec50c2c4b30eb417e7c884d27be4de5e772
ADITYA TRIPATHI <99478857+aditya28tripathi@users.noreply.github.com>
2023-10-25 14:11:51 +0530

    Add files via upload

---
+++ Kashish porfolio/index.js
@@ -0,0 +1,13 @@
+let typed = new Typed(".multiple-text", {
+    strings: [
+      "Frontend Developer",
+      "UI/UX Designer",
+      "Problem Solver",
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+  });
+  
+  
